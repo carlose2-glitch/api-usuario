2c399c5378743470b62fd602fa3d39e9151b2943
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-11-25 11:10:32 -0400

    ajuste de la cookie para que pueda recibir cualquier peticion en produccion

--- src/users/createUser.ts
+++ src/users/createUser.ts
@@ -50,6 +50,7 @@ export class CreateUserEvaluate {
            response.cookie('jwt', token,{
             expires: new Date( Date.now() + 1000 * 60 * 2),
             httpOnly: true,
+            sameSite: 'lax'
           })
 
            return {

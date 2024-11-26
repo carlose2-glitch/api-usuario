8c58aef947185d9ef69e058077455782150783c8
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-11-26 18:29:58 -0400

    prueba cookie 21

--- src/users/createUser.ts
+++ src/users/createUser.ts
@@ -51,7 +51,6 @@ export class CreateUserEvaluate {
            response.cookie('jwt', token,{
             expires: new Date(Date.now() + 1000 * 60 * 5),
             httpOnly: true,
-            sameSite: 'none',
             domain: undefined,
             secure:false
     
